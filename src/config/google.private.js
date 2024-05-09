1af66c082eb2c03cd4ae3de412892f3c630ab1d7
Administrador <Administrador@POLLUXC04>
2024-05-09 12:35:53 -0300

    ajusto login con google3

--- src/config/google.private.js
+++ src/config/google.private.js
@@ -2,6 +2,5 @@ module.exports = {
     appId: '887664',
     clientIDGoogle: '532470277181-3dlmskno7tfsppr6fglo0q1382tg3pqc.apps.googleusercontent.com',
     clientSecretGoogle: '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',
-    callbackUrlGoogle: 'http://localhost:8080/api/sessions/googlecallback',
-    pepe:''
+    callbackUrlGoogle: 'http://localhost:8080/api/sessions/googlecallback'    
 }
