b65edbe14b2b134228ca091137e9e6e3cf4ad287
Administrador <Administrador@POLLUXC04>
2024-05-10 08:15:27 -0300

    habilito archivos de config

---
+++ src/config/google.private.js
@@ -0,0 +1,5 @@
+module.exports = {
+    clientIDGoogle: '532470277181-3dlmskno7tfsppr6fglo0q1382tg3pqc.apps.googleusercontent.com',
+    clientSecretGoogle: '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',
+    callbackUrlGoogle: 'http://localhost:8080/api/sessions/googlecallback'
+}
